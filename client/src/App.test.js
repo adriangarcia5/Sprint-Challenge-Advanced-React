eb86c4ccfd0baadaaeb2d824d11fb66eeedc08a4
Adrian-Garcia <agarc5@hotmail.com>
2019-09-06 11:48:11 -0500

    got two tests to work on app.test.js! :D

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getByText } from '@testing-library/dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
 });
+
+test('header appears on the screen', () => {
+  let { getByText} = render(<App />);
+  
+  getByText(/player list/i);
+});
+
+  
